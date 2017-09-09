9081f0e77b94e2283b9ec5dd928db75c66cd9995
Matt Walker <matt.g.d.walker@gmail.com>
2017-09-09 09:43:34 -0400

    Got to state where it can be compiled by `stack build'.

--- executable/Main.hs
+++ executable/Main.hs
@@ -4,22 +4,7 @@
 -- and only use it in the executable. Doing so allows others to use what you
 -- wrote in their libraries.
 
-import qualified Example
 import ClassyPrelude
-import Options.Applicative
-
-data IDoc = IDoc
-  { cmd :: Command
-  , outfile :: Maybe FilePath }
-
-data Command
-texOptions = undefined
-htmlOptions = undefined
-  
-idoc :: Parser IDoc
-idoc = hsubparser
-  ((command "tex" (info texOptions $ progDesc "convert the file to tex.")) <>
-   (command "html" (info htmlOptions $ progDesc "convert the file to html.")))
 
 main :: IO ()
-main = Example.main
+main = return ()
